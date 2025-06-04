8cf77b525968483ee5370c72b9728c5d1fbbc624
ghaithalabed1997 <ghalabed22@gmail.com>
2025-06-05 01:18:07 +0200

    add audio option to Questions Models

--- public/assets/js/custom.js
+++ public/assets/js/custom.js
@@ -1,104 +1,131 @@
-$(document).on('submit','.database_operation',function(){
-    var url=$(this).attr('action');
-    var data=$(this).serialize();
-    $.post(url,data,function(fb){
-        var resp=$.parseJSON(fb);
-        if(resp.status=='true'){
+$(document).on("submit", ".database_operation", function () {
+    var url = $(this).attr("action");
+    var data = $(this).serialize();
+    $.post(url, data, function (fb) {
+        var resp = $.parseJSON(fb);
+        if (resp.status == "true") {
             alert(resp.message);
             setTimeout(() => {
-                window.location.href=resp.reload;
+                window.location.href = resp.reload;
             }, 1000);
-        }
-        else{
+        } else {
             alert(resp.message);
         }
     });
     return false;
 });
 
-$(document).on('click','.apply_exam',function(){
-    var id=$(this).attr('data-id');
-    $.get(BASE_URL+'/student/apply_exam/'+id,function(fb){
-        var resp=$.parseJSON(fb);
-        if(resp.status=='true'){
+$(document).on("submit", ".database_operation_with_file_upload", function (e) {
+    e.preventDefault();
+
+    var form = this;
+    var url = $(form).attr("action");
+    var formData = new FormData(form);
+
+    $.ajax({
+        url: url,
+        method: "POST",
+        data: formData,
+        contentType: false,
+        processData: false,
+        success: function (fb) {
+            var resp = typeof fb === "string" ? JSON.parse(fb) : fb;
+
+            if (resp.status == "true") {
+                alert(resp.message);
+                setTimeout(() => {
+                    window.location.href = resp.reload;
+                }, 1000);
+            } else {
+                alert(resp.message);
+            }
+        },
+        error: function (xhr) {
+            console.error("Error:", xhr.responseText);
+            alert("An error occurred. Please check the console for details.");
+        },
+    });
+});
+
+$(document).on("click", ".apply_exam", function () {
+    var id = $(this).attr("data-id");
+    $.get(BASE_URL + "/student/apply_exam/" + id, function (fb) {
+        var resp = $.parseJSON(fb);
+        if (resp.status == "true") {
             alert(resp.message);
             setTimeout(() => {
-                window.location.href=resp.reload;
+                window.location.href = resp.reload;
             }, 1000);
-        }
-        else{
+        } else {
             alert(resp.message);
         }
-    })
-})
+    });
+});
 
-$(document).on('click','.category_status',function(){
-    var id=$(this).attr('data-id');
-    $.get(BASE_URL+'/admin/category_status/'+id,function(fb){
+$(document).on("click", ".category_status", function () {
+    var id = $(this).attr("data-id");
+    $.get(BASE_URL + "/admin/category_status/" + id, function (fb) {
         alert("status successsfully changed");
-    })
-})
+    });
+});
 
-
-$(document).on('click','.exam_status',function(){
-    var id=$(this).attr('data-id');
-    $.get(BASE_URL+'/admin/exam_status/'+id,function(fb){
+$(document).on("click", ".exam_status", function () {
+    var id = $(this).attr("data-id");
+    $.get(BASE_URL + "/admin/exam_status/" + id, function (fb) {
         alert("status successsfully changed");
-    })
-})
+    });
+});
 
-$(document).on('click','.student_status',function(){
-    var id=$(this).attr('data-id');
-    $.get(BASE_URL+'/admin/student_status/'+id,function(fb){
+$(document).on("click", ".student_status", function () {
+    var id = $(this).attr("data-id");
+    $.get(BASE_URL + "/admin/student_status/" + id, function (fb) {
         alert("status successsfully changed");
-    })
-})
+    });
+});
 
-$(document).on('click','.portal_status',function(){
-    var id=$(this).attr('data-id');
-    $.get(BASE_URL+'/admin/portal_status/'+id,function(fb){
+$(document).on("click", ".portal_status", function () {
+    var id = $(this).attr("data-id");
+    $.get(BASE_URL + "/admin/portal_status/" + id, function (fb) {
         alert("status successsfully changed");
-    })
-})
+    });
+});
 
-$(document).on('click','.question_status',function(){
-    var id=$(this).attr('data-id');
-    $.get(BASE_URL+'/admin/question_status/'+id,function(fb){
+$(document).on("click", ".question_status", function () {
+    var id = $(this).attr("data-id");
+    $.get(BASE_URL + "/admin/question_status/" + id, function (fb) {
         alert("status successsfully changed");
-    })
-})
-
-
+    });
+});
 
 var interval;
-	function countdown() {
-	  clearInterval(interval);
-	  interval = setInterval( function() {
-	      var timer = $('.js-timeout').html();
-	      timer = timer.split(':');
-	      var minutes = timer[0];
-	      var seconds = timer[1];
-	      seconds -= 1;
-	      if (minutes < 0) return;
-	      else if (seconds < 0 && minutes != 0) {
-	          minutes -= 1;
-	          seconds = 59;
-	      }
-	      else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+function countdown() {
+    clearInterval(interval);
+    interval = setInterval(function () {
+        var timer = $(".js-timeout").html();
+        timer = timer.split(":");
+        var minutes = timer[0];
+        var seconds = timer[1];
+        seconds -= 1;
+        if (minutes < 0) return;
+        else if (seconds < 0 && minutes != 0) {
+            minutes -= 1;
+            seconds = 59;
+        } else if (seconds < 10 && length.seconds != 2) seconds = "0" + seconds;
 
-	      $('.js-timeout').html(minutes + ':' + seconds);
+        $(".js-timeout").html(minutes + ":" + seconds);
 
-	      if (minutes == 0 && seconds == 0) { clearInterval(interval); alert('time UP'); myFunction() }
-	  }, 1000);
-	}
+        if (minutes == 0 && seconds == 0) {
+            clearInterval(interval);
+            alert("time UP");
+            myFunction();
+        }
+    }, 1000);
+}
 
-    var time = document.getElementById('timer').value;
-	$('.js-timeout').text(time);
-	countdown();
+var time = document.getElementById("timer").value;
+$(".js-timeout").text(time);
+countdown();
 
-
-    function myFunction() {
-        document.getElementById("myCheck").click();
-      }
-    
-
+function myFunction() {
+    document.getElementById("myCheck").click();
+}
--- themes/dashboard/views/admin/add_questions.blade.php
+++ themes/dashboard/views/admin/add_questions.blade.php
@@ -1,161 +1,218 @@
 @extends('layouts.app')
-@section('title','Add Questions')
+@section('title', 'Add Questions')
 @section('content')
 
     <!-- /.content-header -->
-     <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Add questions</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Add questions</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 class="m-0">Add questions</h1>
+                    </div><!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Home</a></li>
+                            <li class="breadcrumb-item active">Add questions</li>
+                        </ol>
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
 
-      <section class="content">
-        <div class="container-fluid">
-          <div class="row">
-            <div class="col-12">
-              <!-- Default box -->
-              <div class="card">
-                <div class="card-header">
-                  <h3 class="card-title">Title</h3>
-  
-                  <div class="card-tools">
-                        <a class="btn btn-info btn-sm" href="javascript:;" data-toggle="modal" data-target="#myModal">Add new</a>
-                  </div>
+            <section class="content">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-12">
+                            <!-- Default box -->
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">Title</h3>
+
+                                    <div class="card-tools">
+                                        <a class="btn btn-info btn-sm" href="javascript:;" data-toggle="modal"
+                                            data-target="#myModal">Add new</a>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <table class="table table-striped table-bordered table-hover datatable">
+                                        <thead>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Question</th>
+                                                <th>ans</th>
+                                                <th>Status</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($questions as $key => $question)
+                                                <tr>
+                                                    <td>{{ $key + 1 }}</td>
+                                                    <td>{{ $question['questions'] }}</td>
+                                                    <td>{{ $question['ans'] }}</td>
+                                                    <td><input class="question_status" data-id="{{ $question['id'] }}"
+                                                            <?php if ($question['status'] == 1) {
+                                                                echo 'checked';
+                                                            } ?> type="checkbox" name="status"></td>
+                                                    <td>
+                                                        <a href="{{ url('admin/update_question/' . $question['id']) }}"
+                                                            class="btn btn-primary btn-sm">Update</a>
+                                                        <a href="{{ url('admin/delete_question/' . $question['id']) }}"
+                                                            class="btn btn-danger btn-sm">Delete</a>
+                                                    </td>
+                                                </tr>
+                                            @endforeach
+                                        </tbody>
+                                        <tfoot>
+                                            <tr>
+                                                <th>#</th>
+                                                <th>Question</th>
+                                                <th>ans</th>
+                                                <th>Status</th>
+                                                <th>Actions</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                </div>
+                                <!-- /.card-body -->
+                            </div>
+                            <!-- /.card -->
+                        </div>
+                    </div>
                 </div>
-                <div class="card-body">
-                    <table class="table table-striped table-bordered table-hover datatable">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Question</th>
-                                <th>ans</th>
-                                <th>Status</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                          @foreach ($questions as $key=>$question)
-                              <tr>
-                                  <td>{{ $key+1}}</td>
-                                  <td>{{ $question['questions']}}</td>
-                                  <td>{{ $question['ans']}}</td>
-                                  <td><input class="question_status" data-id="{{ $question['id']}}" <?php if($question['status']==1){ echo "checked";} ?> type="checkbox" name="status"></td>
-                                  <td>
-                                      <a href="{{ url('admin/update_question/'. $question['id'])}}" class="btn btn-primary btn-sm">Update</a>
-                                      <a href="{{ url('admin/delete_question/'. $question['id'])}}" class="btn btn-danger btn-sm">Delete</a>
-                                  </td>
-                              </tr>
-                          @endforeach
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <th>#</th>
-                                <th>Question</th>
-                                <th>ans</th>
-                                <th>Status</th>
-                                <th>Actions</th>
-                            </tr>
-                        </tfoot>
-                    </table>
+            </section>
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Modal -->
+        <div class="modal fade" id="myModal" role="dialog">
+            <div class="modal-dialog modal-lg">
+
+                <!-- Modal content-->
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Add new Question</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <form action="{{ url('/admin/add_new_question') }}" class="database_operation_with_file_upload"
+                            method="POST" enctype="multipart/form-data">
+                            <div class="row">
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <label for="">Enter Question</label>
+                                        {{ csrf_field() }}
+                                        <input type="hidden" name="exam_id" value="{{ Request::segment(3) }}">
+                                        <input type="text" required="required" name="question"
+                                            placeholder="Enter Question" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="">Enter Option 1</label>
+                                        <input type="text" required="required" name="option_1"
+                                            placeholder="Enter Question" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="">Enter Option 2</label>
+                                        <input type="text" required="required" name="option_2"
+                                            placeholder="Enter Option 2" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="">Enter Option 3</label>
+                                        <input type="text" required="required" name="option_3"
+                                            placeholder="Enter  Option 3" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="form-group">
+                                        <label for="">Enter Option 4</label>
+                                        <input type="text" required="required" name="option_4"
+                                            placeholder="Enter  Option 4" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="form-group col-sm-6">
+                                    <div class="flex justify-between items-center w-full">
+                                        <label for="">Select correct option</label>
+                                        <div>
+                                            <select class="form-control" required="required" name="ans">
+                                                <option value="">Select</option>
+
+                                                <option value="option_1">option 1</option>
+                                                <option value="option_2">option 2</option>
+                                                <option value="option_3">option 3</option>
+                                                <option value="option_4">option 4</option>
+
+                                            </select>
+                                        </div>
+
+                                    </div>
+                                </div>
+                                <div class="form-group col-sm-6">
+                                    <div class="flex flex-row justify-between items-center w-full">
+                                        <label for="">Upload Audio</label>
+                                        <div>
+                                            <input type="file" name="audio" accept="audio/mpeg" id="audio_input"
+                                                class="form-control" onchange="previewFile()">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="display-none mt-3 p-2"id="audio_input_wrapper">
+                                    <audio id="audio_preview" controls style="width: 100%; display: none;"></audio>
+                                </div>
+
+                                <div class="col-sm-12">
+                                    <div class="form-group">
+                                        <button class="btn btn-primary">Add</button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </form>
+                    </div>
+
                 </div>
-                <!-- /.card-body -->
-              </div>
-              <!-- /.card -->
             </div>
-          </div>
-        </div>
-      </section>
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Modal -->
-<div class="modal fade" id="myModal" role="dialog">
-    <div class="modal-dialog modal-lg">
-    
-      <!-- Modal content-->
-      <div class="modal-content">
-        <div class="modal-header">
-          <h4 class="modal-title">Add new Question</h4>
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-        </div>
-        <div class="modal-body">
-          <form action="{{ url('/admin/add_new_question')}}" class="database_operation">  
-                <div class="row">
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                            <label for="">Enter Question</label>
-                            {{ csrf_field()}}
-                            <input type="hidden" name="exam_id" value="{{ Request::segment(3)}}">
-                            <input type="text" required="required" name="question" placeholder="Enter Question" class="form-control">
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label for="">Enter Option 1</label>
-                            <input type="text" required="required" name="option_1" placeholder="Enter Question" class="form-control">
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label for="">Enter Option 2</label>
-                            <input type="text" required="required" name="option_2" placeholder="Enter Option 2" class="form-control">
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label for="">Enter Option 3</label>
-                            <input type="text" required="required" name="option_3" placeholder="Enter  Option 3" class="form-control">
-                        </div>
-                    </div>
-                    <div class="col-sm-6">
-                        <div class="form-group">
-                            <label for="">Enter Option 4</label>
-                            <input type="text" required="required" name="option_4" placeholder="Enter  Option 4" class="form-control">
-                        </div>
-                    </div>
-                    {{-- <div class="col-sm-12">
-                        <div class="form-group">
-                            <label for="">Enter correct ans</label>
-                            <input type="text" required="required" name="ans" placeholder="Enter  correct ans" class="form-control">
-                        </div>
-                    </div> --}}
-                    <div class="form-group">
-                      <label for="">Select correct option</label>
-                      <select class="form-control" required="required" name="ans">
-                          <option value="">Select</option>
-                        
-                          <option value="option_1">option 1</option>
-                          <option value="option_2">option 2</option>
-                          <option value="option_3">option 3</option>
-                          <option value="option_4">option 4</option>
-          
-                      </select>
-                  </div>
-
-                    <div class="col-sm-12">
-                        <div class="form-group">
-                            <button class="btn btn-primary">Add</button>
-                        </div>
-                    </div>
-                </div>
-            </form>
-      </div>
-      
-    </div>
-    </div>	
 
 
- 
-@endsection
+
+        @endsection
+        <script>
+            function previewFile() {
+                const file = document.getElementById("audio_input").files[0];
+                const maxSize = 10 * 1024 * 1024; // 10 MB
+
+                if (file.size > maxSize) {
+                    alert("Die Datei ist zu groß. Maximal erlaubt: 10MB.");
+                    document.getElementById("audio_input").value = "";
+                    return;
+                }
+                console.log(file.type);
+
+
+                const preview = document.getElementById("audio_preview");
+                const wrapper = document.getElementById("audio_input_wrapper");
+
+                if (file && file.type.startsWith('audio/')) {
+                    const reader = new FileReader();
+
+                    reader.onload = function(e) {
+                        preview.src = e.target.result;
+                        preview.style.display = 'block';
+                        wrapper.style.display = 'block';
+                        preview.oncanplaythrough = () => {
+                            preview.play().catch(err => console.warn("Autoplay failed:", err));
+                        };
+                    };
+
+                    reader.readAsDataURL(file);
+                }
+            }
+        </script>
